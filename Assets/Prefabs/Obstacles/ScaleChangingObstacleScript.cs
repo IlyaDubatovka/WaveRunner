977404f8575b83548f5a88c59abe73d835a57f02
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-06-27 00:09:18 +0300

    feat: add scale changing obstacle script

--- Assets/Prefabs/Obstacles/ScaleChangingObstacleScript.cs
+++ Assets/Prefabs/Obstacles/ScaleChangingObstacleScript.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ScaleChangingObstacleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _minScaleModificator;
+    [SerializeField] private float _maxScaleModificator;
+    [SerializeField] private float _minSpeedTransformation;
+    [SerializeField] private float _maxSpeedTransformation;
+    private Transform _transform;
+    private float _scaleModificator;
+    private float _speedTransformation;
+
+    private void Awake()
     {
-        
+        _transform = GetComponent<Transform>();
+        _scaleModificator = Random.Range(_minScaleModificator, _maxScaleModificator);
+        _speedTransformation = Random.Range(_minSpeedTransformation, _maxSpeedTransformation);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var scale = _transform.localScale;
+        scale.x = Mathf.PingPong(Time.time * _speedTransformation, _scaleModificator);
+        scale.y = Mathf.PingPong(Time.time * _speedTransformation, _scaleModificator);
+        _transform.localScale = scale;
     }
 }
